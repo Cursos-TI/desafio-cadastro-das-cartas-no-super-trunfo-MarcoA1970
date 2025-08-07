bbe6ea91381d81e09e8adbe404bfacb75280a978
MarcoA1970 <marco_madeira@yahoo.com>
2025-08-07 12:23:38 +0000

    Novato - quase pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 struct CartaSuperTrunfo {
-   char estado;                //Letra do estado
+   char estado[3];                //Sigla do estado
    char codigo[4];            //Codigo da carta
    char nomeCidade[100];      //Nome da cidade
    int populacao;             //População da cidade
@@ -23,8 +23,8 @@ int main () {
 
     printf ("Cadastro da Carta 1:\n");
 
-    printf ("Digite a letra do Estado (A-H): ");
-    scanf (" %c", &carta1.estado);
+    printf ("Digite a sigla do Estado (Ex: MG): ");
+    scanf (" %2s", carta1.estado);
 
     printf ("Digite o código da carta (ex: A01) : ");
     scanf (" %s", carta1.codigo);
@@ -38,7 +38,7 @@ int main () {
     printf ("Digite a area (km²): ");
     scanf ("%f", &carta1.area);
 
-    printf ("Digite o PIB (em reais): ");
+    printf ("Digite o PIB (em bilhões de reais): ");
     scanf ("%f", &carta1.pib);
 
     printf ("Digite o número de pontos turísticos: ");
@@ -48,8 +48,8 @@ int main () {
 
     printf ("Cadastro da Carta 2:\n");
 
-    printf ("Digite a letra do Estado (A-H): ");
-    scanf (" %c", &carta2.estado);
+     printf ("Digite a sigla do Estado (Ex: MG): ");
+    scanf (" %2s", carta2.estado);
 
     printf ("Digite o código da carta (ex: A01) : ");
     scanf (" %s", carta2.codigo);
@@ -63,7 +63,7 @@ int main () {
     printf ("Digite a area (km²): ");
     scanf ("%f", &carta2.area);
 
-    printf ("Digite o PIB (em reais): ");
+    printf ("Digite o PIB (em bilhões de reais): ");
     scanf ("%f", &carta2.pib);
 
     printf ("Digite o número de pontos turísticos: ");
@@ -71,28 +71,22 @@ int main () {
 
     //impressão Carta 1
     printf ("\ncarta 1:\n");
-    printf ("Estado: %c\n", carta1.estado);
+    printf ("Estado: %s\n", carta1.estado);
     printf ("Código: %s\n", carta1.codigo);
     printf ("Nome da cidade: %s\n", carta1.nomeCidade);
     printf ("População: %d\n", carta1.populacao);
-    printf ("Área: %2f km²\n", carta1.area);
-    printf ("PIB: %.2fbilhoes de reais\n", carta1.pib);
+    printf ("Área: %.2f km²\n", carta1.area);
+    printf ("PIB: %.2f bilhoes de reais\n", carta1.pib);
     printf ("Número de pontos turísticos: %d\n", carta1.pontosTuristicos);
 
     //impressão Carta 2
     printf ("\ncarta 2:\n");
-    printf ("Estado: %c\n", carta2.estado);
+    printf ("Estado: %s\n", carta2.estado);
     printf ("Código: %s\n", carta2.codigo);
     printf ("Nome da cidade: %s\n", carta2.nomeCidade);
     printf ("População: %d\n", carta2.populacao);
-    printf ("Área: %2f km²\n", carta2.area);
-    printf ("PIB: %.2fbilhoes de reais\n", carta2.pib);
+    printf ("Área: %.2f km²\n", carta2.area);
+    printf ("PIB: %.2f bilhoes de reais\n", carta2.pib);
     printf ("Número de pontos turísticos: %d\n", carta2.pontosTuristicos);
 
-
-
-
-        
-
-
-}
+     }
