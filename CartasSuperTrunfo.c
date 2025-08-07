c4538ae8bdab7a89ba4df93a92e3da1af0c24e0c
MarcoA1970 <marco_madeira@yahoo.com>
2025-08-07 01:04:50 +0000

    QUASE PRONTO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -69,6 +69,29 @@ int main () {
     printf ("Digite o número de pontos turísticos: ");
     scanf ("%d", &carta2.pontosTuristicos);
 
+    //impressão Carta 1
+    printf ("\ncarta 1:\n");
+    printf ("Estado: %c\n", carta1.estado);
+    printf ("Código: %s\n", carta1.codigo);
+    printf ("Nome da cidade: %s\n", carta1.nomeCidade);
+    printf ("População: %d\n", carta1.populacao);
+    printf ("Área: %2f km²\n", carta1.area);
+    printf ("PIB: %.2fbilhoes de reais\n", carta1.pib);
+    printf ("Número de pontos turísticos: %d\n", carta1.pontosTuristicos);
+
+    //impressão Carta 2
+    printf ("\ncarta 2:\n");
+    printf ("Estado: %c\n", carta2.estado);
+    printf ("Código: %s\n", carta2.codigo);
+    printf ("Nome da cidade: %s\n", carta2.nomeCidade);
+    printf ("População: %d\n", carta2.populacao);
+    printf ("Área: %2f km²\n", carta2.area);
+    printf ("PIB: %.2fbilhoes de reais\n", carta2.pib);
+    printf ("Número de pontos turísticos: %d\n", carta2.pontosTuristicos);
+
+
+
+
         
 
 
